2c0237c505b78e5f0c8a9e0df0def0ecb59fe2d5
Azamat Sh <128055521+azamat0910@users.noreply.github.com>
2024-03-20 01:13:53 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './components/heading/header';
+import Divider from './components/heading/divider';
+import Form from './components/input';
+
+// import AddTask from './components/addTask';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Header/>
+      <Divider/>
+      <Form/>
+
+    
+    </div>
+  );
+}
+
+export default App;
