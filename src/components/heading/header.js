3e9444370ed9a30faf49573ef7100b011f6e5974
Azamat Sh <128055521+azamat0910@users.noreply.github.com>
2024-03-20 01:17:06 +0500

    Add files via upload

---
+++ src/components/heading/header.js
@@ -0,0 +1,12 @@
+import './header.css'
+ 
+export default function Header () {
+    return (
+        <>
+            <header> 
+                <h1> TO-DO NOW </h1> 
+            </header>
+            
+        </>
+    )
+}
